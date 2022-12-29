9a1dee62adedfe92aecaf52caa7557ae97b4a6ab
Chris Marron <chrisluk04@gmail.com>
2022-12-29 05:43:33 +0000

    pause

--- js/main.js
+++ js/main.js
@@ -23,3 +23,29 @@ function formEvent(e) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+function renderEntry(entry) {
+  var ulParent = document.createElement('ul');
+  ulParent.setAttribute('class', 'row');
+  var test2 = document.createElement('li');
+  test2.setAttribute('class', 'column-half');
+  ulParent.prepend(test2);
+  var test3 = document.createElement('img');
+  test3.setAttribute('src', entry.entries.photo);
+  test2.appendChild(test3);
+  var liSibling = document.createElement('li');
+  liSibling.setAttribute('class', 'column-half');
+  ulParent.appendChild(liSibling);
+  var h2li = document.createElement('h2');
+  h2li.textContent = entry.entries.title;
+  liSibling.prepend(h2li);
+  return ulParent;
+}
+var test = document.querySelector('.hidden');
+document.addEventListener('DOMContentLoaded', function (e) {
+  for (var i in data.entries) {
+    var tes19 = renderEntry(data.entries[i]);
+    test.appendChild(tes19);
+
+  }
+});
